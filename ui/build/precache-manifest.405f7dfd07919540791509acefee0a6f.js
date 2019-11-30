c03521ba8e8c9bdb3d5fd145d33304197d43e433
ClauDev <claudwatari@gmail.com>
2019-11-30 18:53:29 +0300

    fix-addBuildFiles

---
+++ ui/build/precache-manifest.405f7dfd07919540791509acefee0a6f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb13f89dba6a20aea78ec81aefe733b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05d977c5f82b95a25c48",
+    "url": "/static/css/main.219e2346.chunk.css"
+  },
+  {
+    "revision": "8db26d70f8c718058227",
+    "url": "/static/js/2.0345b2c8.chunk.js"
+  },
+  {
+    "revision": "05d977c5f82b95a25c48",
+    "url": "/static/js/main.f105bac7.chunk.js"
+  },
+  {
+    "revision": "2b42684769868e6e20fd",
+    "url": "/static/js/runtime-main.835aaafc.js"
+  }
+]);
